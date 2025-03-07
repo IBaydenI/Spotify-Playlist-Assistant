baab73a8e216421eac549948e34ab7cae6bb9046
Calvin <calvin@example.com>
2025-03-07 10:25:19 +0100

    fix and refactor spa/authorization.py

--- spa/authorization.py
+++ spa/authorization.py
@@ -8,6 +8,8 @@ import string
 import hashlib
 import base64
 import requests
+import urllib
+import webbrowser
 
 """
 Input: length of the string to be generated
@@ -19,24 +21,38 @@ def generateRandomString(length):
         random_string += (secrets.choice(string.ascii_letters + string.digits + "-" + "_" + "."))
     return random_string
 
+def getCodeChallenge():
+    codeVerifier = generateRandomString(64)
+    codeVerifier_hashed = hashlib.sha256(codeVerifier.encode('utf-8')).digest()
+    codeVerifier_base64 = base64.urlsafe_b64encode(codeVerifier_hashed).decode("utf-8")
+    codeVerifier_base64 = codeVerifier_base64.replace('=', '')
+    return codeVerifier_base64
+
 def SpotifyAuthorization():
     clientID = "" # From Spotify developer portal
+    redirectUri = 'http://127.0.0.1:8080'
+    state = None
     authorizationEndpoint = "https://accounts.spotify.com/authorize"
     tokenEndpoint = "https://accounts.spotify.com/api/token"
-    scope = 'playlist-read-private playlist-read-collaborative playlist-modify-private playlist-modify-public'
+    # TO-DO! fix passing scope in get request (currently error message is "Illegal scope")
+    scope = ("playlist-read-private", "playlist-read-collaborative", "playlist-modify-private", "playlist-modify-public")
 
-    codeVerifier = generateRandomString(64)
-    codeVerifier_hashed = hashlib.sha256(codeVerifier.encode('ascii')).digest()
-    codeVerifier_base64 = base64.b64encode(codeVerifier_hashed).decode("ascii")
+    code_challenge = getCodeChallenge()
 
-    params = {
-        "response_type": 'code',
+    payload = { # parameters for get request
         "client_id": clientID,
-        "scope": scope,
+        "response_type": 'code',
+        "redirect_uri": redirectUri,
         "code_challenge_method": 'S256',
-        "code_challenge": codeVerifier_base64
+        "code_challenge": code_challenge,
+        "state": state,
     }
-    getReq = requests.get(authorizationEndpoint, params)
-    print(getReq.status_code)
+    urlparams = urllib.parse.urlencode(payload)
+    getReq = requests.get(authorizationEndpoint, params=urlparams) # send get request and save response
+
+    # open URL returned from request in webbrowser. This is necessary for user authentication
+    webbrowser.open_new(getReq.url)
+
+
 
 SpotifyAuthorization()
